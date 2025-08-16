745e3d21fec950ec2611bd57a06f4447511342b3
victorortiz <victorandresortiz@gmail.com>
2025-08-16 00:49:34 -0600

    Basic Wireframe for file upload completed

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+
+
+    <div class="d-flex justify-content-center align-items-center ">
+        <div class = "container-fluid upload-box">
+            <label for="formFileLg" class="form-label">Upload your documentation</label>
+            <input class="form-control form-control-lg" id="formFileLg" type="file">
+        </div>
+    </div>
+    
+
+</template>
+
+<style scoped>
+
+.upload-box{
+    width: 800px;
+    height: 500px;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <script setup>
+import FileUpload from '@/components/FileUpload.vue';
+
 
 </script>
 
 <template>
+    
 
-   <h1>
-   
-   </h1>
+    <h1 class="text-center my-5" >
+        Fix my Doc
+    </h1>
+
+
+
+ <FileUpload></FileUpload>
 
 </template>
